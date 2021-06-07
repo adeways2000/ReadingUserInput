29c2ad99931bbfb4850ddd14179ff77623e2d944
ayokunle adeleye <adeways2000@gmail.com>
2021-06-07 11:52:14 +0200

    Initial commit

---
+++ src/com/company/Scanner.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Scanner {
+    public Scanner(java.io.InputStream in) {
+    }
+
+    public java.lang.String nextLine() {
+        return null;
+    }
+
+
+    public void close() {
+    }
+
+    public int nextIn() {
+        return 0;
+    }
+
+    public int nextInt() {
+        return 0;
+    }
+
+    public boolean hasNextInt() {
+        return false;
+    }
+
+}
